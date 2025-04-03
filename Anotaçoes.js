a835d14f38bee5d6595b40a9a90b2b2e2e738384
AnaLeticiaRocha <analeticiadru2007@gmail.com>
2025-04-03 11:35:42 -0400

    hello world

---
+++ Anotaçoes.js
@@ -0,0 +1,22 @@
+// var 'é possivel redeclarar o nome da variavel' , let 'variavel, manipulável' ou const 'constante, não muda o valor'
+//number var valor=20;
+//valor.toFixed(2) '20.00'
+//boolean var ativo= true
+//ativo.to String(); 'true'
+// string var nome = hello
+//nome.toLowerCase(); 'para letras minuscula'
+//typeof antes do nome da variavel var valor=4; 
+//console.log (typeof valor) 
+//propriedades não tem parenteses, função tem
+// array 'coleção de dados, podemos armazenar mais de um valor em uma variavel'
+//let estados = ['Rio de Janeiro', 'São Paulo', 'Bahia']; 
+//estados [0] 'acessar o primeiro valor'
+// estados [2] = 'C' 'para mudar o valor'
+// let vaga = ['programador php', 5, true]; 'Pode ter mais de um tipo de valor'
+//.length para contar
+// let linguagens = ['JavaScript', 'PHP', 'C#', 'JAVA']
+//console.log(linguagens.length);
+//let telefones = [(11) 98899 - 8787, (22)3455 - 8819, (91) 9562 - 0000] 'armazena varios valores'
+//undefined 'valor indefinido'
+// null 'não atribuir valor para a variavel'
+//let nome=null;
